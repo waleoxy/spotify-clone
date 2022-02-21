9c261c187cca63546374fa26bdf6f95c8dc337ca
Olawale Otubu <waleoxy@gmail.com>
2022-02-21 16:47:08 +0100

    initial commit, main page setup and nextautj impl

---
+++ components/Sidebar.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import {
+  HeartIcon,
+  HomeIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  RssIcon,
+  SearchIcon,
+} from '@heroicons/react/outline'
+
+const Sidebar = () => {
+  return (
+    <div className="border-r border-gray-900 p-5 text-xs text-gray-500">
+      <div className="space-y-4">
+        <button className="flex items-center space-x-2 hover:text-white ">
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white ">
+          <SearchIcon className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white ">
+          <LibraryIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+        <button className="flex items-center space-x-2 hover:text-white ">
+          <PlusCircleIcon className="h-5 w-5" />
+          <p>Create Playlists</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white ">
+          <HeartIcon className="h-5 w-5" />
+          <p>On Episodes</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white ">
+          <LibraryIcon className="h-5 w-5" />
+          <p>Liked Songs</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+        {/*playlist*/}
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+        <p className="cursor-pointer hover:text-white">Playlist... name</p>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ pages/login.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const login = () => {
+  return <div> this is the login page</div>
+}
+
+export default login
