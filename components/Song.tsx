1d483417268edbf6eb6d70c7351f92f6656104a0
Olawale Otubu <waleoxy@gmail.com>
2022-02-24 15:35:46 +0100

    creation of time conversion helper and tweakn of song ui

--- components/Song.tsx
+++ components/Song.tsx
@@ -1,6 +1,7 @@
 import { shuffle } from 'lodash'
 import React, { useEffect, useState } from 'react'
 import useSpotify from '../hooks/useSpotify'
+import { millisToMinutesAndSeconds } from '../lib/time'
 
 const Song = ({ track, order }) => {
   const spotifyApi = useSpotify()
@@ -13,7 +14,7 @@ const Song = ({ track, order }) => {
   }, [track.track])
 
   return (
-    <div>
+    <div className="grid grid-cols-2">
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <img
@@ -22,9 +23,13 @@ const Song = ({ track, order }) => {
           alt="album pix"
         />
         <div>
-          <p>{track.track.name}</p>
+          <p className="w-36 truncate md:w-64">{track.track.name}</p>
           <p>{track.track.artists[0].name}</p>
         </div>
+      </div>
+      <div className="ml-auto flex items-center justify-between md:ml-0">
+        <p className="hidden md:inline">{track.track.album.name}</p>
+        <p>{millisToMinutesAndSeconds(track.track.duration_ms)}</p>
       </div>
     </div>
   )
